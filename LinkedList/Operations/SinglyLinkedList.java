ff6109e8b123eb4ada1ddf0f32acf05d5928bd63
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-05-28 22:50:11 +0530

    Add SinglyLinkedList

---
+++ LinkedList/Operations/SinglyLinkedList.java
@@ -0,0 +1,190 @@
+public class Create 
+{
+  public static void main(String[] args) 
+  {
+    SinglyLinkedList s = new SinglyLinkedList();
+    s.createSinglyLinkedList(5);
+    s.insertInLinkedList(6, 1);
+    s.insertInLinkedList(7, 3);
+    s.insertInLinkedList(8, 4);
+    s.insertInLinkedList(9, 5);
+    s.traverseLinkedList();
+    s.searchNode(8);
+  }
+}
+
+
+class Node 
+{
+  int value;
+  Node next;
+}
+
+
+class SinglyLinkedList 
+{
+  Node head;
+  Node tail;
+  int size;
+
+  // Create LinkedList
+  public Node createSinglyLinkedList(int nodeValue) 
+  {
+    head = new Node();
+    Node node = new Node();
+    node.next = null;
+    node.value = nodeValue;
+    head = node;
+    tail = node;
+    size = 1;
+    return head;
+  }
+
+
+  // Insertion
+  public void insertInLinkedList(int nodeValue, int location)
+  {
+    Node node = new Node();
+    node.value = nodeValue;
+    
+    if(head == null) 
+    {
+      createSinglyLinkedList(nodeValue);
+      return;
+    }
+
+    else if(location == 0)
+    {
+      node.next = head;
+      head = node;
+    }
+
+    else if(location >= size)
+    {
+      node.next = null;
+      tail.next = node;
+      tail = node;
+    }
+
+    else
+    {
+      Node tempNode = head;
+      int index = 0;
+      while(index < location - 1)
+      {
+        tempNode = tempNode.next;
+        index++;
+      }
+      Node nextNode = tempNode.next;
+      tempNode.next = node;
+      node.next = nextNode;
+    }
+    size++;
+  }
+
+
+  // LinkedList Traversal
+  public void traverseLinkedList()
+  {
+    
+    if(head == null)
+    {
+      System.out.println("Linked List does not exist!");
+    }
+    
+    else
+    {
+      Node tempNode = head;
+      for(int i=0; i<size; i++)
+      {
+        System.out.print(tempNode.value);
+        if(i != size - 1)
+        {
+          System.out.print(" -> ");
+        }
+        tempNode = tempNode.next;
+      }
+    }
+    System.out.println("\n");
+  }
+
+
+  // Searching 
+  public boolean searchNode(int nodeValue)
+  {
+    if(head != null)
+    {
+      Node tempNode = head;
+      for(int i=0; i<size; i++)
+      {
+        if(tempNode.value == nodeValue)
+        {
+          System.out.println("Node found at Location - " + i);
+          return true;
+        }
+        tempNode = tempNode.next;
+      }
+    }
+    System.out.println("Node not found!");
+    return false;
+  }
+
+
+  // Delete Node
+  public void deleteNode(int location)
+  {
+    if(head == null) 
+    {
+      System.out.println("Linked List does not exist!");
+      return;
+    } 
+    
+    else if(location == 0)
+    {
+      head = head.next;
+      size--;
+      if(size == 0) 
+      {
+        tail = null;
+      }
+    }
+
+    else if(location >= size)
+    {
+      Node tempNode = head;
+      for(int i=0; i<size-1; i++)
+      {
+        tempNode = tempNode.next;
+      }
+      if(tempNode == head)
+      {
+        tail = head = null;
+        size--;
+        return;
+      }
+      tempNode.next = null;
+      tail = tempNode;
+      size--;
+    }
+
+    else
+    {
+      Node tempNode = head;
+      for(int i=0; i<location-1; i++)
+      {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = tempNode.next.next;
+      size--;
+    }
+  }
+
+
+  // Delete the Entire LinkedList
+  public void deleteEntireLinkedList()
+  {
+    head = null;
+    tail = null;
+    System.out.println("LinkedList Deleted Successfully!");
+  }
+}
