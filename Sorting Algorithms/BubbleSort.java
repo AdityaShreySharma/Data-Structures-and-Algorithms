854e129aebd851b86203f2fd35f8748d1ac178fa
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-06-09 00:04:07 +0530

    Refactor Code

--- Sorting Algorithms/BubbleSort.java
+++ Sorting Algorithms/BubbleSort.java
@@ -12,7 +12,7 @@ public class BubbleSort
   public void bubbleSort(int arr[])
   {
     int n = arr.length;
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n-1; i++)
     {
       for(int j=0; j<n-i-1; j++)
       {
