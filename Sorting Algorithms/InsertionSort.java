18f4cf547f764791cd3fcb036fe772b0bb393374
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-06-09 10:43:26 +0530

    Refactor Code

--- Sorting Algorithms/InsertionSort.java
+++ Sorting Algorithms/InsertionSort.java
@@ -10,7 +10,17 @@ public class InsertionSort
   }
   public void insertionSort(int arr[])
   {
-    
+    for(int i=1; i<arr.length; i++)
+    {
+      int temp = arr[i];
+      int j = i;
+      while(j > 0 && arr[j-1] > temp)
+      {
+        arr[j] = arr[j-1];
+        j--;
+      }
+      arr[j] = temp;
+    }
   }
 
   public void displayArray(int arr[])
