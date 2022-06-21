1dbc6291a6859aaa6349b2cd72bc79cdcfc7ae9a
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-06-21 10:08:09 +0530

    Refactor Code

---
+++ Dynamic Programming/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Fibonacci
+{
+  public static void main(String args[])
+  {
+    HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+    System.out.println(fib(6, h));
+  }
+
+  public static int fib(int n, HashMap<Integer, Integer> h)
+  {
+    if(n == 1)
+    {
+      return 0;
+    }
+    if(n == 2)
+    {
+      return 1;
+    }
+    if(!h.containsKey(n))
+    {
+      h.put(n, (fib(n - 1, h) + fib(n - 2, h)));
+    }
+    return h.get(n);
+  }
+}
