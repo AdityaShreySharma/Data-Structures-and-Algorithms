9b458d7cd7de8ec4dc5557bdb420a6d44b99862e
Aditya Shrey Sharma <shrey2100@Adityas-MacBook-Air.local>
2022-06-27 23:45:07 +0530

    Refactor Code

---
+++ Arrays/Kadane's Algorithm.java
@@ -0,0 +1,23 @@
+public class Kadane
+{
+  public static void main(String[] args)
+  {
+    int arr[] = {1, 2, 3, -2, 5};
+    System.out.println(maximumSubarraySum(arr));
+  }
+
+  public static int maximumSubarraySum(int arr[])
+  {
+    int maxSum = Integer.MIN_VALUE;
+    int currentSum = 0;
+    for(int i=0; i<arr.length; i++) 
+    {
+      currentSum += arr[i];
+      if(currentSum > maxSum) 
+      {
+        maxSum = currentSum;
+      } 
+    }
+    return maxSum;
+  }
+}
