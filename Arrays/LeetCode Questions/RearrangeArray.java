e27f494abe234d85609576fb4991829786458785
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-07-01 12:30:22 +0530

    Refactor Code

--- Arrays/LeetCode Questions/RearrangeArray.java
+++ Arrays/LeetCode Questions/RearrangeArray.java
@@ -1,61 +1,38 @@
+import java.util.*;
 public class RearrangeArray
 {
   public static void main(String[] args)
   {
-    RearrangeArray ra = new RearrangeArray();
-
     int arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8};
-    ra.rearrangeArray(arr);
-    ra.displayArray(arr);
+    rearrangeArray(arr);
+    System.out.println(Arrays.toString(arr));
   }
 
-  public void rearrangeArray(int arr[])
+  public static int partitionArray(int arr[])
   {
-    int outOfPlace = -1;
+    int j = 0;
+    int pivot = 0;
     for(int i = 0; i < arr.length; i++)
     {
-      if(outOfPlace >= 0)
+      if(arr[i] < pivot)
       {
-        if((arr[i] >= 0 && arr[outOfPlace] < 0) || (arr[i] < 0 && arr[outOfPlace] >= 0))
-        {
-          rightRotate(arr, outOfPlace, i);
-          if(i - outOfPlace >= 2)
-          {
-            outOfPlace += 2; 
-          }
-          else
-          { 
-            outOfPlace = -1;
-          }
-        }
-      }
-
-      if(outOfPlace == -1)
-      { 
-        if((arr[i] >= 0 && (i & 0x01) == 0) || (arr[i] < 0 && (i & 0x01) == 1))
-        {
-          outOfPlace = i;
-        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j++;
       }
     }
+    return j;
   }
 
-  public void rightRotate(int arr[], int outOfPlace, int current)
+  public static void rearrangeArray(int arr[])
   {
-    int temp = arr[current];
-    for(int i = current; i > outOfPlace; i--)
+    int partition = partitionArray(arr);
+    for(int i = 0; (partition < arr.length && i < partition); partition++, i += 2)
     {
-      arr[i] = arr[i - 1];
+      int temp = arr[i];
+      arr[i] = arr[partition];
+      arr[partition] = temp;
     }
-    arr[outOfPlace] = temp;
-  }
-
-  public void displayArray(int arr[])
-  {
-    for(int i = 0; i < arr.length; i++)
-    {
-      System.out.print(arr[i] + " ");
-    }
-    System.out.println(" ");
   }
 }
