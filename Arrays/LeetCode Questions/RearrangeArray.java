5bd5b53be2266766e08d274f847b07234d9e825c
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-07-01 12:55:12 +0530

    Refactor Code

--- Arrays/LeetCode Questions/RearrangeArray.java
+++ Arrays/LeetCode Questions/RearrangeArray.java
@@ -9,10 +9,9 @@ public class RearrangeArray
   public static int partitionArray(int arr[])
   {
     int j = 0;
-    int pivot = 0;
     for(int i = 0; i < arr.length; i++)
     {
-      if(arr[i] < pivot)
+      if(arr[i] < 0)
       {
         int temp = arr[i];
         arr[i] = arr[j];
