4dabb5dbef53fff5d493f7474e6a17e04cbb8a1e
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-07-01 12:46:40 +0530

    Refactor Code

--- Arrays/LeetCode Questions/RearrangeArray.java
+++ Arrays/LeetCode Questions/RearrangeArray.java
@@ -1,11 +1,9 @@
-import java.util.*;
 public class RearrangeArray
 {
   public static void main(String[] args)
   {
     int arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8};
     rearrangeArray(arr);
-    System.out.println(Arrays.toString(arr));
   }
 
   public static int partitionArray(int arr[])
@@ -34,5 +32,11 @@ public class RearrangeArray
       arr[i] = arr[partition];
       arr[partition] = temp;
     }
+
+    for(int i = 0; i < arr.length; i++)
+    {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println(" ");
   }
-}
+} 
