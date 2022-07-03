95d65fea587063ca0f4bbed1edacbb0b464b6199
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-07-04 00:17:09 +0530

    Refactor Code

---
+++ Arrays/LeetCode Questions/TripletSum.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class TripletSum
+{
+  public static void main(String[] args)
+  { 
+    int arr[] = {1, 4, 45, 6, 10, 8,};
+    int sum = 13;
+    tripletSum(arr, sum);
+  }
+
+  public static void tripletSum(int arr[], int sum)
+  {
+    Arrays.sort(arr);
+    int n = arr.length;
+
+    for(int i = 0; i < n - 3; i++)
+    { 
+      int k = sum - arr[i];
+      int low = i + 1;
+      int high = arr.length - 1;
+      while(low < high)
+      {
+        if(arr[low] + arr[high] < k)
+        {
+          low++;
+        }
+        else if(arr[low] + arr[high] > k)
+        {
+          high--;
+        }
+        else
+        { 
+          System.out.println("(" + arr[i] + ", " + arr[low] + ", " + arr[high] + ")");
+          low++;
+          high--;
+        }
+      }
+    }
+  }
+}
