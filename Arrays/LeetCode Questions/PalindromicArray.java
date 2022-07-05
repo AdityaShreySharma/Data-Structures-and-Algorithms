b6c154c2a022df369a20694123de8cbd9c82a263
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-07-05 13:02:59 +0530

    Refactor Code

--- Arrays/LeetCode Questions/PalindromicArray.java
+++ Arrays/LeetCode Questions/PalindromicArray.java
@@ -12,7 +12,7 @@ public class PalindromicArray
     int len = str.length();
     for(int i = 0; i < (len / 2); i++) 
     {
-      if(str.charAt(i) != str.charAt(len - 1- i))
+      if(str.charAt(i) != str.charAt(len - 1 - i))
       {
         return false;
       }
