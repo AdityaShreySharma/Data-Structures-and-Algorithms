57047e9f99f5cc1af12cf67c740b7612671dc686
Aditya Shrey Sharma <adityashrey2000@gmail.com>
2022-07-29 00:22:39 +0530

    Refactor Code

--- Matrix/SpecificPair.java
+++ Matrix/SpecificPair.java
@@ -16,9 +16,9 @@ class SpecificPair
   {
     int n = matrix.length;
     int maxValue = Integer.MIN_VALUE;
-    int maxArray[][] = new int[n][n];
+    int dp[][] = new int[n][n];
 
-    maxArray[n - 1][n - 1] = matrix[n - 1][n - 1];
+    dp[n - 1][n - 1] = matrix[n - 1][n - 1];
 
     int max = matrix[n - 1][n - 1];
     for(int j = n - 2; j >= 0; j--)
@@ -27,7 +27,7 @@ class SpecificPair
       {
         max = matrix[n - 1][j];
       }
-      maxArray[n - 1][j] = max;
+      dp[n - 1][j] = max;
     }
 
     max = matrix[n - 1][n - 1];
@@ -37,18 +37,18 @@ class SpecificPair
       { 
         max = matrix[i][n - 1];
       }
-      maxArray[i][n - 1] = max;
+      dp[i][n - 1] = max;
     }
 
     for(int i = n - 2; i >= 0; i--)
     { 
       for(int j = n - 2; j >= 0; j--)
       {
-        if((maxArray[i + 1][j + 1] - matrix[i][j]) > maxValue)
+        if((dp[i + 1][j + 1] - matrix[i][j]) > maxValue)
         {
-          maxValue = maxArray[i + 1][j + 1] - matrix[i][j];
+          maxValue = dp[i + 1][j + 1] - matrix[i][j];
         }
-        maxArray[i][j] = Math.max(matrix[i][j], Math.max(maxArray[i][j + 1], maxArray[i + 1][j]));
+        dp[i][j] = Math.max(matrix[i][j], Math.max(dp[i][j + 1], dp[i + 1][j]));
       }
     }
     return maxValue;
